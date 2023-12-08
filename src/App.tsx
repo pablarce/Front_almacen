e3bc0b65a922f9c2212f7755eba8b84e31333dcb
pabloArce2 <pabloarceproyects@gmail.com>
2023-12-08 18:33:43 +0100

    Navbar init

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar className="flex items-center"/>
     </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,15 @@
-const Navbar = () => {
+import { useState } from "react"
+import logo from "../assets/logo.svg"
+
+interface NavbarProps{
+    className? : string
+}
+
+const Navbar = (props: NavbarProps) => {
     return(
-        <div>
-            <p>hola</p>
+        <div className={`${props.className} gap-2 px-10 py-6`}>
+            <img width= "100px" src={logo} alt="" />
+            <p className=" text-2xl">Almacenes Manolo</p>
         </div>
     )
 }
