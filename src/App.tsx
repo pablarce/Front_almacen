689726bc34942234ac9b479fe9f12551e4fdb51b
pabloArce2 <pabloarceproyects@gmail.com>
2023-12-09 00:51:09 +0100

    styles + dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -26,10 +26,10 @@ function App() {
   return (
     <div className=" overflow-hidden">
       <Navbar className="flex items-center h-40"/>
-      <div style={{ height: `${screenHeight - 160}px` }} className={`grid grid-cols-5 gap-6 px-10 w-full pb-10`}>
-          <Articles className="bg-gray-100 p-4 col-span-2 border-2 rounded-xl w-full h-full"/>
-          <Product className="bg-gray-100 p-4 col-span-3 border-2 rounded-xl w-full h-full"/>
-          <Bill className="bg-gray-100 p-4 border-2 col-span-5 rounded-xl w-full h-full"/>
+      <div style={{ height: `${screenHeight - 160}px` }} className={`grid grid-cols-2 gap-6 px-10 w-full pb-10`}>
+          <Articles className="bg-gray-100 p-4 border-2 rounded-xl overflow-y-scroll"/>
+          <Product className="bg-gray-100 p-4 border-2 rounded-xl w-full"/>
+          <Bill className="bg-gray-100 p-4 border-2 col-span-2 rounded-xl w-full h-full"/>
       </div>
     </div>
   );
--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -1,32 +1,20 @@
-import data from "../assets/data.json";
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table"
+import { MoreHorizontal } from "lucide-react"
 
-import {
-    ColumnDef,
-    flexRender,
-    getCoreRowModel,
-    useReactTable,
-  } from "@tanstack/react-table"
-   
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "../components/ui/table"
+import data from "../assets/data.json"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../components/ui/table"
 
-interface ArticleProps{
-    className? : string
+interface ArticleProps {
+    className?: string
 }
 
 type Product = {
-    id: string;
-    product: string;
-    stock: number;
-    price: number;
-};
-  
+    id: string
+    product: string
+    stock: number
+    price: number
+}
+
 const columns: ColumnDef<Product>[] = [
     {
         accessorKey: "id",
@@ -39,65 +27,72 @@ const columns: ColumnDef<Product>[] = [
     {
         accessorKey: "stock",
         header: "Stock",
+        cell: ({ row }) => {
+            const stock = parseFloat(row.getValue("stock"))
+
+            return <p className="items-center">{stock} uds</p>
+        },
     },
     {
         accessorKey: "price",
         header: "Price",
+        cell: ({ row }) => {
+            const amount = parseFloat(row.getValue("price"))
+            const formatted = new Intl.NumberFormat("en-ES", {
+                style: "currency",
+                currency: "EUR",
+            }).format(amount)
+
+            return <p>{formatted}</p>
+        },
     },
-  ];
+]
 
 const Articles = (props: ArticleProps) => {
-
     const table = useReactTable({
         data,
         columns,
         getCoreRowModel: getCoreRowModel(),
-      });
+    })
 
-    return(
-        <div className={`rounded-md border ${props.className}`}>
-          <Table>
-            <TableHeader>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <TableRow key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => {
-                    return (
-                      <TableHead key={header.id}>
-                        {header.isPlaceholder
-                          ? null
-                          : flexRender(
-                              header.column.columnDef.header,
-                              header.getContext()
-                            )}
-                      </TableHead>
-                    )
-                  })}
-                </TableRow>
-              ))}
-            </TableHeader>
-            <TableBody>
-              {table.getRowModel().rows?.length ? (
-                table.getRowModel().rows.map((row) => (
-                  <TableRow
-                    key={row.id}
-                    data-state={row.getIsSelected() && "selected"}
-                  >
-                    {row.getVisibleCells().map((cell) => (
-                      <TableCell key={cell.id}>
-                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                      </TableCell>
+    return (
+        <div className={`${props.className}`}>
+            <Table>
+                <TableHeader>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => {
+                                return (
+                                    <TableHead key={header.id}>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(header.column.columnDef.header, header.getContext())}
+                                    </TableHead>
+                                )
+                            })}
+                        </TableRow>
                     ))}
-                  </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell colSpan={columns.length} className="h-24 text-center">
-                    No results.
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
+                </TableHeader>
+                <TableBody>
+                    {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map((row) => (
+                            <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                                {row.getVisibleCells().map((cell) => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))
+                    ) : (
+                        <TableRow>
+                            <TableCell colSpan={columns.length} className="h-24 text-center">
+                                No results.
+                            </TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
         </div>
     )
 }
